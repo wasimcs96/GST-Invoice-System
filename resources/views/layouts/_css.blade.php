22f3d43186c0b8a7a1b8d5f657c59ae1617f7a8f
saifjatu56 <saifjatu56@gmail.com>
2021-06-19 19:12:24 +0530

    changes

--- resources/views/layouts/_css.blade.php
+++ resources/views/layouts/_css.blade.php
@@ -1,13 +1,34 @@
 <!-- Stylesheets -->
-<link type="text/css" href="{{ asset('assets/vendor/simplebar.min.css') }}" rel="stylesheet">
-<link type="text/css" href="{{ asset('assets/css/app.css?v=1.0.1') }}" rel="stylesheet">
-<link type="text/css" href="{{ asset('assets/css/vendor-material-icons.css') }}" rel="stylesheet">
-<link type="text/css" href="{{ asset('assets/css/vendor-fontawesome-free.css') }}" rel="stylesheet">
-<link type="text/css" href="{{ asset('assets/css/vendor-select2.css') }}" rel="stylesheet">
-<link type="text/css" href="{{ asset('assets/vendor/select2/select2.min.css') }}" rel="stylesheet">
-<link type="text/css" href="{{ asset('assets/css/vendor-flatpickr.css') }}" rel="stylesheet">
-<link type="text/css" href="{{ asset('assets/css/vendor-flatpickr-airbnb.css') }}" rel="stylesheet">
-<link type="text/css" href="{{ asset('assets/css/vendor-bootstrap-image-checkbox.css') }}" rel="stylesheet">
+
+<link rel="apple-touch-icon" href="../../../app-assets/images/ico/apple-icon-120.png">
+    <link rel="shortcut icon" type="image/x-icon" href="../../../app-assets/images/ico/favicon.ico">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
+
+    <!-- BEGIN: Vendor CSS-->
+    <link rel="stylesheet" type="text/css" href="{{asset('theme/app-assets/vendors/css/vendors.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('theme/app-assets/vendors/css/charts/apexcharts.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('theme/app-assets/vendors/css/extensions/toastr.min.css')}}">
+    <!-- END: Vendor CSS-->
+
+    <!-- BEGIN: Theme CSS-->
+    <link rel="stylesheet" type="text/css" href="{{asset('theme/app-assets/css/bootstrap.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('theme/app-assets/css/bootstrap-extended.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('theme/app-assets/css/colors.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('theme/app-assets/css/components.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('theme/app-assets/css/themes/dark-layout.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('theme/app-assets/css/themes/bordered-layout.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('theme/app-assets/css/themes/semi-dark-layout.css')}}">
+
+    <!-- BEGIN: Page CSS-->
+    <link rel="stylesheet" type="text/css" href="{{asset('theme/app-assets/css/core/menu/menu-types/vertical-menu.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('theme/app-assets/css/pages/dashboard-ecommerce.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('theme/app-assets/css/plugins/charts/chart-apex.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('theme/app-assets/css/plugins/extensions/ext-component-toastr.css')}}">
+    <!-- END: Page CSS-->
+
+    <!-- BEGIN: Custom CSS-->
+    <link rel="stylesheet" type="text/css" href="{{asset('theme/assets/css/style.css')}}">
+    <!-- END: Custom CSS-->
 
 @if(get_system_setting('application_favicon'))
 <!-- Favicon -->
--- resources/views/layouts/_js.blade.php
+++ resources/views/layouts/_js.blade.php
@@ -1,23 +1,32 @@
 <!-- Application Scripts -->
-<script src="{{ asset('assets/vendor/jquery.min.js') }}"></script>
-<script src="{{ asset('assets/vendor/popper.min.js') }}"></script>
-<script src="{{ asset('assets/vendor/bootstrap.min.js') }}"></script>
-<script src="{{ asset('assets/vendor/simplebar.min.js') }}"></script>
-<script src="{{ asset('assets/vendor/dom-factory.js') }}"></script>
-<script src="{{ asset('assets/vendor/material-design-kit.js') }}"></script>
-<script src="{{ asset('assets/js/toggle-check-all.js') }}"></script>
-<script src="{{ asset('assets/js/check-selected-row.js') }}"></script>
-<script src="{{ asset('assets/js/dropdown.js') }}"></script>
-<script src="{{ asset('assets/js/sidebar-mini.js') }}"></script>
-<script src="{{ asset('assets/vendor/select2/select2.min.js') }}"></script>
-<script src="{{ asset('assets/js/select2.js') }}"></script>
-<script src="{{ asset('assets/js/jquery.priceformat.min.js') }}"></script>
-<script src="{{ asset('assets/vendor/flatpickr/flatpickr.min.js') }}"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+<!-- BEGIN: Vendor JS-->
+<script src="{{asset('theme/app-assets/vendors/js/vendors.min.js')}}"></script>
+    <!-- BEGIN Vendor JS-->
 
-<script src="{{ asset('assets/js/app.js') }}"></script>
-<script src="{{ asset('assets/js/custom.js?v=1.0.2') }}"></script>
+    <!-- BEGIN: Page Vendor JS-->
+    <script src="{{asset('theme/app-assets/vendors/js/charts/apexcharts.min.js')}}"></script>
+    <script src="{{asset('theme/app-assets/vendors/js/extensions/toastr.min.js')}}"></script>
+    <!-- END: Page Vendor JS-->
 
+    <!-- BEGIN: Theme JS-->
+    <script src="{{asset('theme/app-assets/js/core/app-menu.js')}}"></script>
+    <script src="{{asset('theme/app-assets/js/core/app.js')}}"></script>
+    <!-- END: Theme JS-->
+
+    <!-- BEGIN: Page JS-->
+    <script src="{{asset('theme/app-assets/js/scripts/pages/dashboard-ecommerce.js')}}"></script>
+    <!-- END: Page JS-->
+
+    <script>
+        $(window).on('load', function() {
+            if (feather) {
+                feather.replace({
+                    width: 14,
+                    height: 14
+                });
+            }
+        })
+    </script>
 @yield('page_body_scripts')
 
 
