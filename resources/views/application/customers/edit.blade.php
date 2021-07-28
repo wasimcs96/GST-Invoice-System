cf73ce1a5e196a8144e650ba70550897b1392648
Mohammed saif <saifchohan7235@gmail.com>
2021-07-28 18:03:38 +0530

    sdzds

--- resources/views/application/customers/edit.blade.php
+++ resources/views/application/customers/edit.blade.php
@@ -25,3 +25,89 @@
         @include('application.customers._form')
     </form>
 @endsection
+
+@section('page_body_scripts')
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('.oi').on('change', function() {
+            
+            var stateID = $(this).val();
+            console.log(stateID);
+            console.log("s")
+            if(stateID) {
+            console.log("s")
+
+                $.ajax({
+                    url: "{{ route('application.customer.city') }}",
+                    type: "GET",
+                    data: { id:stateID },
+                    success:function(data) {
+                    //    console.log(data);
+                        
+                        $('select[name="billing[city]"]').empty();
+                        $.each(data, function(key, value) {
+                            // console.log(value.name);
+                            $('select[name="billing[city]"]').append('<option value="'+ value.id +'">'+ value.name +'</option>');
+                        });
+
+
+                    }
+                });
+            }else{
+                $('select[name="city"]').empty();
+            }
+        });
+    });
+</script>
+
+<script>
+    $(function () {
+    var $src = $('#billing_name'),
+        $dst = $('#shipping_name');
+    $src.on('input', function () {
+        $dst.val($src.val());
+    });
+}); 
+
+$(function () {
+    var $src1 = $('#billing_phone'),
+        $dst1 = $('#shipping_phone');
+    $src1.on('input', function () {
+        $dst1.val($src1.val());
+    });
+}); 
+
+$(function () {
+    var $src2 = $('#billing_country_id'),
+        $dst2 = $('#shipping_country_id');
+    $src2.on('input', function () {
+        $dst2.val($src2.val());
+    });
+}); 
+
+$(function () {
+    var $src3 = $('#billing_state'),
+        $dst3 = $('#shipping_state');
+    $src3.on('input', function () {
+        $dst3.val($src3.val());
+    });
+}); 
+
+$(function () {
+    var $src4 = $('#billing_city'),
+        $dst4 = $('#shipping_city');
+    $src4.on('input', function () {
+        $dst4.val($src4.val());
+    });
+}); 
+
+$(function () {
+    var $src5 = $('#billing_address'),
+        $dst5 = $('#shipping_address');
+    $src5.on('input', function () {
+        $dst5.val($src5.val());
+    });
+}); 
+</script>
+
+@endsection
